23831a74311cf4b519107f2956c921b33bdffa3e
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-05-28 16:20:30 -0300

    Adiciona Exercicio 1

---
+++ MaiorDeIdade.java
@@ -0,0 +1,11 @@
+public class MaiorDeIdade {
+    public static void main(String[] args) {
+        int idade = 8;
+
+        if(idade >= 18){
+            System.out.println("Você é maior de idade");
+        }else{
+            System.out.println("Você é Menor de idade");
+        }
+    }
+}
